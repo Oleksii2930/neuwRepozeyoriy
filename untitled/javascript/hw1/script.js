d660615c008805adc1af8663623b53904fe4de52
Олексій2930 <korobcukdenis30@gmail.com>
2025-07-20 20:48:27 +0300

    dz1-1 js

---
+++ untitled/javascript/hw1/script.js
@@ -0,0 +1,37 @@
+// let a = "hello";
+// let b = "owu";
+// let c = "com";
+// let d = "ua";
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let i = 3.14;
+// let j = 2.7;
+// let k = 16;
+// let l = true;
+// let m = false;
+// console.log(a,b,c,d,e,f,g,h,i,j,k,l,m);
+
+
+let firstName = "Oleksiy";
+let middleName = "Romanovich";
+let lastName = "Zborivskiy";
+
+console.log(firstName + " " + middleName + " " + lastName);
+
+let a = 100;
+let b = "100";
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+let name = prompt("Введіть ваше ім'я:");
+let patronymic = prompt("Введіть ваше по батькові:");
+let age = prompt("Скільки вам років?");
+
+console.log("Ім'я: " + name);
+console.log("По батькові: " + patronymic);
+console.log("Вік: " + age + " років");
